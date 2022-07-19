2e504a230cae1de30408ff602398101e5f1baf6c
marcin <makuchmarcin0@gmail.com>
2022-07-19 22:38:41 +0200

    codewars kyu 7

--- src/com/company/codewars/kyu7/SurviveTheZombies.java
+++ src/com/company/codewars/kyu7/SurviveTheZombies.java
@@ -6,7 +6,16 @@ public class SurviveTheZombies {
     }
 
     public static String zombieShootout(int zombies, int range, int ammo) {
-
-        return null;
+        String ending = "";
+        if (range * 2 > zombies && ammo > zombies) {
+            ending = "You shot all " + zombies + " zombies.";
+        }
+        if (range * 2 > zombies && ammo < zombies) {
+            ending = "You shot " + ammo + " zombies before being eaten: ran out of ammo.";
+        }
+        if (range * 2 < zombies && ammo > zombies) {
+            ending = "You shot " + zombies + " zombies before being eaten: overwhelmed.";
+        }
+        return ending;
     }
 }
