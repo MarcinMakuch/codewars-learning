17e66de722fdd37c638802299e583961e6016fdd
marcin <makuchmarcin0@gmail.com>
2022-07-19 23:05:50 +0200

    codewars kyu 7

--- src/com/company/codewars/kyu7/SurviveTheZombies.java
+++ src/com/company/codewars/kyu7/SurviveTheZombies.java
@@ -7,15 +7,21 @@ public class SurviveTheZombies {
 
     public static String zombieShootout(int zombies, int range, int ammo) {
         String ending = "";
+        int shootFired = 0;
+        int zombiesShoot = 0;
         if (range * 2 > zombies && ammo > zombies) {
             ending = "You shot all " + zombies + " zombies.";
         }
-        if (range * 2 > zombies && ammo < zombies) {
-            ending = "You shot " + ammo + " zombies before being eaten: ran out of ammo.";
-        }
-        if (range * 2 < zombies && ammo > zombies) {
-            ending = "You shot " + zombies + " zombies before being eaten: overwhelmed.";
+        for (int i = 1; i <= range * 2; i++) {
+            zombiesShoot++;
+            if (zombiesShoot == ammo) {
+                ending = "You shot " + ammo + " zombies before being eaten: ran out of ammo.";
+            }
         }
         return ending;
     }
 }
+
+//ending="You shot all "+zombies+" zombies.";
+//ending="You shot "+ammo+" zombies before being eaten: ran out of ammo.";
+//ending="You shot "+zombies+" zombies before being eaten: overwhelmed.";
