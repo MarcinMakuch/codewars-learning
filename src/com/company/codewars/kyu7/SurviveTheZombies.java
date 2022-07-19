468e574c2fb72fadd29428cef696803962be4709
marcin <makuchmarcin0@gmail.com>
2022-07-19 22:26:26 +0200

    codewars kyu 7

---
+++ src/com/company/codewars/kyu7/SurviveTheZombies.java
@@ -0,0 +1,12 @@
+package com.company.codewars.kyu7;
+
+public class SurviveTheZombies {
+    public static void main(String[] args) {
+
+    }
+
+    public static String zombieShootout(int zombies, int range, int ammo) {
+
+        return null;
+    }
+}
