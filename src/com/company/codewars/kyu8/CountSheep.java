f3eeadb6056bffe056c96dedb5987d80a8c1f01b
marcin <makuchmarcin0@gmail.com>
2022-07-21 23:07:08 +0200

    codewars kyu 8

---
+++ src/com/company/codewars/kyu8/CountSheep.java
@@ -0,0 +1,15 @@
+package com.company.codewars.kyu8;
+
+public class CountSheep {
+    public static void main(String[] args) {
+
+    }
+
+    public static String countingSheep(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= num; i++) {
+            sb.append(i).append(" sheep...");
+        }
+        return sb.toString();
+    }
+}
