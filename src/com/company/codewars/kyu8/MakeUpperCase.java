b10288d0861d7c5126967f7dce755191d9b0ba3a
marcin <makuchmarcin0@gmail.com>
2022-07-21 22:59:15 +0200

    codewars kyu 8

---
+++ src/com/company/codewars/kyu8/MakeUpperCase.java
@@ -0,0 +1,10 @@
+package com.company.codewars.kyu8;
+
+public class MakeUpperCase {
+    public static void main(String[] args) {
+
+    }
+    public static String MakeUpperCase(String str){
+        return str.toUpperCase();
+    }
+}
